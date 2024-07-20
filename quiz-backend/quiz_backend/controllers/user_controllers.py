82cc10b34050275cf396fada624e56b9e9cdd896
SYED RAZA ALI <144608213+Syed-Raza-Ali@users.noreply.github.com>
2024-07-20 17:41:21 +0500

    User Controllers

--- quiz-backend/quiz_backend/controllers/user_controllers.py
+++ quiz-backend/quiz_backend/controllers/user_controllers.py
@@ -1,14 +1,20 @@
-from quiz_backend.utils.imports import (User , Token , UserModel, select, Session, passwordintoHash)
+from quiz_backend.utils.imports import (User , Token , UserModel, select, Session, passwordintoHash,verifyPassword)
 from quiz_backend.utils.exception import ConflictException
 
 def signUp(user_form: UserModel, session : Session):
-    # TODO: verify id user is already exist
+    users = session.exec(select(User).where(User.user_email == user_form.user_email)).all()
     hashed_password = passwordintoHash(user_form.user_password)
-    
-    user_exist = session.exec(select(User).where(User.user_email == user_form.user_email)).all()
-    if user_exist:
-        raise ConflictException("email")
-    
+
+    for user in users :
+        is_email_exist = user.user_email == user_form.user_email
+        is_password_exist = verifyPassword(user.user_password, user_form.user_password) 
+        if is_email_exist and is_password_exist :
+            raise ConflictException("email and password")
+        elif is_email_exist :
+            raise ConflictException("email")
+        elif is_password_exist :
+            raise ConflictException("password")
+     
     user = User(user_name=user_form.user_name, user_email=user_form.user_email,user_password=hashed_password)
     session.add(user)
     session.commit()    
